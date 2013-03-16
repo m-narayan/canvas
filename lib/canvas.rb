5b89355405f28c23e04a47aa82bd621b8ee19ca4
sukumarreddy <sukumar@arrivusystems.com>
2013-03-16 11:18:06 +0530

    commit

--- lib/canvas.rb
+++ lib/canvas.rb
@@ -9,7 +9,7 @@ require 'json'
       puts "hello"
     end
 
-    def initialize())
+    def initialize()
       @@oauth_token = "DBzxyOO2xzYNwNbjgjEh4MCxtgYbKTfS4gRTWuDSskY3H1LnzHECJhygZN0RW73h"
       @@api_root_url = "https://192.168.1.40/"
     end
